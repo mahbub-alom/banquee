7a521b713154270ea42947eb3a6dc54b333ecea2
mahbub-alom <mdmahbuba034@gmail.com>
2025-03-14 16:28:43 +0600

    nav bar create done

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,10 +1,83 @@
-import React from 'react'
+"use client"; 
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FaAngleDown } from "react-icons/fa";
 
 const NavBar = () => {
-  return (
-    <div><button className="btn btn-warning">Neutral</button></div>
-    
-  )
-}
+  const pathname = usePathname();
 
-export default NavBar
+  const isActive = (path: string) =>
+    pathname === path
+      ? "bg-gray-200 text-black transition-all duration-300"
+      : "hover:bg-gray-100 transition-all duration-300";
+  
+
+  const navItem = (
+    <>
+      <li>
+        <Link href="/feature" className={`p-2 rounded ${isActive("/feature")}`}>
+          Feature
+        </Link>
+      </li>
+      <li>
+        <Link href="/compare" className={`p-2 rounded ${isActive("/compare")}`}>
+          Compare <FaAngleDown />
+        </Link>
+      </li>
+      <li>
+        <Link href="/support" className={`p-2 rounded ${isActive("/support")}`}>
+          Support
+        </Link>
+      </li>
+      <li>
+        <Link href="/blog" className={`p-2 rounded ${isActive("/blog")}`}>
+          Blog <FaAngleDown />
+        </Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-white shadow-sm md:px-20">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content rounded-box z-1 mt-3 w-28 p-2 shadow text-black bg-white"
+          >
+            {navItem}
+          </ul>
+        </div>
+        <Link href="/" className="text-3xl font-bold text-[#5BB5A2]">
+          banquee.
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-black">{navItem}</ul>
+      </div>
+      <div className="navbar-end gap-5">
+        <button className="text-[#5bb5a2] font-bold">Login</button>
+        <button className="btn btn-active bg-[#5bb5a2] text-white font-light">Open Account</button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
